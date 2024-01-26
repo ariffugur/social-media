da056b10d0cb2be611033ce345e3d470761f37f0
ariffugur <ariffugur@gmail.com>
2024-01-26 09:43:41 +0300

    Database and repository layer update

---
+++ src/main/java/com/ariffugur/socialmedia/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.ariffugur.socialmedia.repository;
+
+import com.ariffugur.socialmedia.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
